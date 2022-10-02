459152794d60d1c671e17146d23790a46474d3ac
Alecx <99165584+Zumbulw@users.noreply.github.com>
2022-10-02 05:55:34 +0300

    Vue projektin alku

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <KomponenttiTesti msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import KomponenttiTesti from './components/KomponenttiTesti.vue'
+
+export default {
+  name: 'App',
+  components: {
+    KomponenttiTesti
+  }
+}
+</script>
---
+++ src/components/KomponenttiTesti.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="[&>*]:m-4">
+    <h1 class="bg-red-600 w-[10rem]">a</h1>
+    <h1 class="bg-green-600">b</h1>
+    <h1 class="bg-blue-600">c</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'KomponenttiTesti',
+  }
+  </script>
+  
